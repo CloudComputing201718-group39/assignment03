8965dbe0df4d11208aca4b2478a9114dc0a14fb9
Stefan Su <stfn.su@live.de>
2017-12-11 13:28:14 +0100

    Implement product-description-service with jQuery approach

--- product-descp-service/product_descp.js
+++ product-descp-service/product_descp.js
@@ -1,10 +1,26 @@
+/**
+ * This service returns based on the productid the product name and the URL.
+ * 
+ */
 module.exports = function (options) {
+    var seneca = this;
     //Import the mock data json file
     const mockData = require('./MOCK_DATA.json');
 
-    //To DO: Add the patterns and their corresponding functions
+    //TODO: Add the patterns and their corresponding functions
+    seneca.add('role:product_descp,cmd:productid', (msg, reply) => {
+        var res = $.grep(mockData, (entry) => {
+            return entry.product_id == msg.product_id;
+        });
+        reply(null, {"product_id": res.product_id, "productURL": res.product_url});
+    })
 
+    //TODO: add the pattern functions and describe the logic inside the function
+    seneca.act({role: 'product_descp', cmd: 'productid'}, () => {
 
-    //To DO: add the pattern functions and describe the logic inside the function
+    })
 
+    function find_productid () {
+
+    }
 }
