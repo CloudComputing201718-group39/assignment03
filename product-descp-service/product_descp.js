7b26a826683cf70e0df1e7c249c4934598993c31
Stefan Su <stfn.su@live.de>
2017-12-15 19:06:53 +0100

    Add buggy action patterns to product-description-service microservice

--- product-descp-service/product_descp.js
+++ product-descp-service/product_descp.js
@@ -3,19 +3,49 @@
  * 
  */
 module.exports = function (options) {
-    let seneca = this;
-
-    //Import the mock data json file
+    // Import the mock data json file
     const mockData = require('./MOCK_DATA.json');
 
-    //TODO: Add the patterns and their corresponding functions
-    seneca.add('role:product_descp,cmd:productid', (msg, reply) => {
-        let res = $.grep(mockData, (entry) => {
-            return entry.product_id === msg.product_id;
-        });
-        reply(null, {"product_id": res.product_id, "productURL": res.product_url});
-    });
+    // let find = function (id) {
+    //     for (i = 0; i < mockData.length; i++) {
+    //         if (mockData[i].product_id === id)
+    //             return mockData[i];
+    //     }
+    // };
 
-    //TODO: add the pattern functions and describe the logic inside the function
-    seneca.act('role: product_descp cmd: productid product_id: 1', console.log);
-}
+    // TODO: Add the patterns and their corresponding functions
+    this
+        .add(
+            {role:'product_descp', cmd:'productname'},
+            (msg, respond) => {
+                for (i = 0; i < mockData.length; i++) {
+                    if (mockData[i].product_id === parseInt(msg.product_id)) {
+                        respond(null, {productName: mockData[i].product_name}); // TODO: Fix this 'cannot read property 'product_id' of undefined'
+                    }
+                }
+            }
+         );
+
+    this
+        .add(
+            {role:'product_descp', cmd:'producturl'},
+            (msg, respond) => {
+                for (i = 0; i < mockData.length; i++) {
+                    if (mockData[i].product_id === parseInt(msg.product_id)) {
+                        respond(null, {productName: mockData[i].product_url}); // TODO: Fix this 'cannot read property 'product_id' of undefined'
+                    }
+                }
+            }
+        );
+
+    // this.add({role:'product_descp', cmd:'producturl'}, (msg, reply) => {
+    //     var result = find(mockData, (x) => {
+    //         return x.product_id === msg.product_id
+    //     });
+    //
+    //     reply(null, {product_URL: result.product_url})
+    // });
+
+    // TODO: add the pattern functions and describe the logic inside the function
+    this.act({role: 'product_descp', cmd: 'productname', product_id: '1'}, console.log);
+};
