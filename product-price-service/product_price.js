256d555cfc35c2d8aee6f33be39b3ce63d9ad85a
Stefan Su <stfn.su@live.de>
2017-12-18 14:11:37 +0100

    Add pattern and pattern function for product price service
    Fix syntax error

--- product-price-service/product_price.js
+++ product-price-service/product_price.js
@@ -1,9 +1,26 @@
+/**
+ * This service returns based on the productid the product price.
+ *
+ */
 module.exports = function (options) {
     //Import the mock data json file
     const mockData = require('./MOCK_DATA.json');
-    //To DO: Add the patterns and their corresponding functions
 
+    // Todo: Add the patterns and their corresponding functions
+    this.add('role:product_price, cmd:proudctprice', getProductPrice);
 
-    //To DO: add the pattern functions and describe the logic inside the function
+    // Todo: add the pattern functions and describe the logic inside the function
+    function getProductPrice(msg, respond) {
+        if (msg.productID) {
+            for (i = 0; i < mockData.length; i++) {
+                if (mockData[i].product_id === parseInt(msg.productId)) {
+                    respond(null, { productPrice: mockData[i].product_price });
+                }
+            }
+        }
+        else {
+            respond(null, { productPrice: ''});
+        }
+    }
 
 }
