026f6801a0fe73263991dcb5574e3959f709b48e
Stefan Su <stfn.su@live.de>
2017-12-18 13:57:50 +0100

    Separate pattern functions from patterns

--- product-descp-service/product_descp.js
+++ product-descp-service/product_descp.js
@@ -6,46 +6,36 @@ module.exports = function (options) {
     // Import the mock data json file
     const mockData = require('./MOCK_DATA.json');
 
-    // let find = function (id) {
-    //     for (i = 0; i < mockData.length; i++) {
-    //         if (mockData[i].product_id === id)
-    //             return mockData[i];
-    //     }
-    // };
-
     // TODO: Add the patterns and their corresponding functions
-    this
-        .add(
-            {role:'product_descp', cmd:'productname'},
-            (msg, respond) => {
-                for (i = 0; i < mockData.length; i++) {
-                    if (mockData[i].product_id === parseInt(msg.product_id)) {
-                        respond(null, {productName: mockData[i].product_name}); // TODO: Fix this 'cannot read property 'product_id' of undefined'
-                    }
-                }
-            }
-         );
-
-    this
-        .add(
-            {role:'product_descp', cmd:'producturl'},
-            (msg, respond) => {
-                for (i = 0; i < mockData.length; i++) {
-                    if (mockData[i].product_id === parseInt(msg.product_id)) {
-                        respond(null, {productName: mockData[i].product_url}); // TODO: Fix this 'cannot read property 'product_id' of undefined'
-                    }
-                }
-            }
-        );
-
-    // this.add({role:'product_descp', cmd:'producturl'}, (msg, reply) => {
-    //     var result = find(mockData, (x) => {
-    //         return x.product_id === msg.product_id
-    //     });
-    //
-    //     reply(null, {product_URL: result.product_url})
-    // });
+    this.add({role:'product_descp', cmd:'productname'}, getProductName);
+    this.add({role:'product_descp', cmd:'producturl'}, getProductURL);
 
     // TODO: add the pattern functions and describe the logic inside the function
-    this.act({role: 'product_descp', cmd: 'productname', product_id: '1'}, console.log);
+    function getProductName(msg, respond) {
+        if (msg.productID) {
+            for (i = 0; i < mockData.length; i++) {
+                if (mockData[i].product_id === parseInt(msg.productId)) {
+                    respond(null, { productName: mockData[i].product_name });
+                }
+            }
+        }
+        else {
+            respond(null, { productName: ''});
+        }
+    }
+
+    function getProductURL(msg, respond) {
+        if (msg.productID) {
+            for (i = 0; i < mockData.length; i++) {
+                if (mockData[i].product_id === parseInt(msg.productId)) {
+                    respond(null, { productURL: mockData[i].product_url });
+                }
+            }
+        }
+        else {
+            respond(null, { productURL: ''});
+        }
+    }
+
+    // this.act({role: 'product_descp', cmd: 'productname', product_id: '1'}, console.log);
 };
